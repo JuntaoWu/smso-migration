5cade31ff186a1e0a95e7bfbd724536cd8ea10f7
Tony Juntao. Wu <wujuntaocn@outlook.com>
2019-03-08 15:55:51 +0800

    Add project files.

---
+++ smso-migration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace smso_migration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
