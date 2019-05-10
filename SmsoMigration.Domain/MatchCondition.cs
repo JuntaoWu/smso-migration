99bbae2c47167c4c4394e5dd39becae1145b6546
Tony Juntao. Wu <wujuntaocn@outlook.com>
2019-05-10 18:46:45 +0800

    data_data_history migration.

---
+++ SmsoMigration.Domain/MatchCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmsoMigration.Domain
+{
+    public class MatchCondition
+    {
+        public const string ConditionEquals = "equals";
+        public const string ConditionStartsWith = "startswith";
+        public const string ConditionContains = "contains";
+    }
+}
